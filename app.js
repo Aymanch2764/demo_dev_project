4c1795f8293baf29e52ad817ded406ceff7eb390
Aymanch2764 <f223652@cfd.nu.edu.pk>
2026-05-07 02:24:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function divide(a, b) {
   if (b === 0) {
     throw new Error("Cannot divide by zero!");
   }
-  return a / b;
+  return a + b;
 }
 
 module.exports = { add, subtract, multiply, divide };
